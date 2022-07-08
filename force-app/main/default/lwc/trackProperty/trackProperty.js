17605f1357be66072c71f1297b0d52ea3682f1bc
Serpil Atis <serpilatis@gmail.com>
2022-07-07 22:05:54 -0500

    TrackProperty

---
+++ force-app/main/default/lwc/trackProperty/trackProperty.js
@@ -0,0 +1,13 @@
+import { LightningElement, track } from 'lwc';
+
+export default class TrackProperty extends LightningElement {
+    @track location = {
+        city: "Houston",
+        country: "United States",
+        postalCode: "50021"
+    };
+
+    handleChange(event) {
+        this.location.city = event.target.value;
+    }
+}
